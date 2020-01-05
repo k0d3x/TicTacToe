22903ccb67064860ca77285fd96f40b3dba9c230
Rahul Yadav <devmethyl@outlook.com>
2020-01-05 14:46:55 +0530

    Initial commit

---
+++ Classes/Definitions.h
@@ -0,0 +1,12 @@
+//
+// Created by Rahul Yadav on 2020-01-04.
+//
+
+#ifndef PROJ_ANDROID_DEFINITIONS_H
+#define PROJ_ANDROID_DEFINITIONS_H
+
+
+
+#define MAIN_MENU_BG "HD1280/HelloWorld.png"
+
+#endif //PROJ_ANDROID_DEFINITIONS_H
